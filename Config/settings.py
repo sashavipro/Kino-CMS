95d3a9eaa3b1aeed8bf17b2d6d2de4728661fdab
Sasha <sashavipro@gmail.com>
2025-10-01 14:06:10 +0000

    code changes #23 -

--- Config/settings.py
+++ Config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = env('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 
 # Application definition
 
